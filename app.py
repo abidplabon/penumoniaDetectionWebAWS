3a8d76c1b9b47a1c24c61e02d99e063bb826bf2c
abidahammed <54079464+abidplabon@users.noreply.github.com>
2022-01-23 23:33:27 +0600

    Takes and stores file locally

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+from uuid import uuid4
+from flask import Flask, render_template, request,send_from_directory
+##import tensorflow as tf
+global graph,model
+##graph = tf.get_default_graph()
+##import detection
+
+
+result = 0
+
+##ml = detection.detectp()
+     
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    # Image folder 
+    target = os.path.join(APP_ROOT, 'images/')
+    # target = os.path.join(APP_ROOT, 'static/')
+    
+    if not os.path.isdir(target):
+            os.mkdir(target)
+
+    # print(request.files.getlist("file"))
+
+    for upload in request.files.getlist("file"):
+        file_name = str(uuid4())
+
+        print(file_name)
+
+        destination = "/".join([target, f'{file_name}.png'])
+        
+        upload.save(destination)
+    
+    ##result = ml.detect(destination)
+    
+
+    return render_template("complete.html",result=round(result*100,3))
+
+@app.route("/upload")
+def send_image():
+    return send_from_directory("images", "heatmap.png")
+
+if __name__ == "__main__":
+    app.run(port=80)
